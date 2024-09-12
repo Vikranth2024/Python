349af03297d41f6842c3d3abd5ffcbb2c3402be9
Vikranth2024 <167829581+Vikranth2024@users.noreply.github.com>
2024-09-12 19:57:12 +0530

    Update 22_leapyear.py

--- 22_leapyear.py
+++ 22_leapyear.py
@@ -1,7 +1,9 @@
 leap_years = []
-
-for year in range(2000, 2401):
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        leap_years.append(year)
-
+for i in range(2000, 2401):
+    if i%4 == 0:
+            if i%100 == 0:
+                if i%400 == 0:
+                    leap_years.append(i)
+            else:
+                leap_years.append(i)        
 print(leap_years)
